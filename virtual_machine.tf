7e908f05a4e29c8944b913d459b5dffce27c37e4
Soham Dabadgaonkar <130518761+Soham-ssd@users.noreply.github.com>
2025-04-14 15:06:58 +0530

    Update virtual_machine.tf

--- virtual_machine.tf
+++ virtual_machine.tf
@@ -14,3 +14,11 @@ resource "azurerm_windows_virtual_machine" "vm_main" {
     name      = "main-vm_disk1_c8ba0cdcced042e2b174e97f0cf84246"
 
   }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+}
