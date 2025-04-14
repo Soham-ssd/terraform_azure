f25668a8d1d6f5ac95c07b67fade80c564b254b7
Soham Dabadgaonkar <130518761+Soham-ssd@users.noreply.github.com>
2025-04-14 15:04:32 +0530

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -4,3 +4,9 @@ resource "azurerm_network_security_group" "nsg_main" {
   location            = azurerm_resource_group.rg_main.location
   resource_group_name = azurerm_resource_group.rg_main.name
 }
+
+# Associate the network security group with the subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_association_main" {
+  subnet_id                 = azurerm_subnet.subnet_main.id
+  network_security_group_id = azurerm_network_security_group.nsg_main.id
+}
